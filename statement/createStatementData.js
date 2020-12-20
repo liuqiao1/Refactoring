df84c7fc0cbb7a0b7ebc18909d4415ccc3530415
liuqiao1 <350473544@qq.com>
2020-12-20 18:31:25 +0800

    finish

--- statement/createStatementData.js
+++ statement/createStatementData.js
@@ -37,47 +37,6 @@ function playFor(aPerformance){
     return plays[aPerformance.playID]
 }
 
-
-class PerformanceCalculator{
-    constructor(aPerformance){
-        this.performance = aPerformance
-        this.play = playFor(aPerformance)
-    }
-
-    get amount(){
-      throw new Error('subclass responsibility')
-    }
-
-    get volumeCredits(){
-        return Math.max(this.performance.audience - 30, 0);
-    }
-}
-
-class TragedyCaculator extends PerformanceCalculator{
-    get amount(){
-        let result = 40000;
-        if (this.performance.audience > 30) {
-            result += 1000 * (this.performance.audience - 30);
-        }
-        return result
-    }
-}
-class ComedyCaculator extends PerformanceCalculator{
-    get amount(){
-        let result = 30000;
-        if (this.performance.audience > 20) {
-          result += 10000 + 500 * (this.performance.audience - 20);
-        }
-        result += 300 * this.performance.audience;
-        return result
-    }
-
-    get volumeCredits(){
-        return super.volumeCredits + Math.floor(this.performance.audience / 5);
-    }
-}
-
-
 function totalAmount(performances){
     return performances
     .reduce((result, perf) => {
