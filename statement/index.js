3641c6e5b5855e48973dcff0cf6772ac7a2bb6c1
liuqiao1 <350473544@qq.com>
2020-12-20 16:24:18 +0800

    aPerformance

--- statement/index.js
+++ statement/index.js
@@ -24,29 +24,29 @@ const invoices = [
     }
   ]
 
-  function playFor(perf){
-      return plays[perf.playID]
+  function playFor(aPerformance){
+      return plays[aPerformance.playID]
   }
 
-  function amountFor(perf){
+  function amountFor(aPerformance){
     let result = 0;
 
-    switch (playFor(perf).type) {
+    switch (playFor(aPerformance).type) {
         case "tragedy":
           result = 40000;
-          if (perf.audience > 30) {
-            result += 1000 * (perf.audience - 30);
+          if (aPerformance.audience > 30) {
+            result += 1000 * (aPerformance.audience - 30);
           }
           break;
         case "comedy":
           result = 30000;
-          if (perf.audience > 20) {
-            result += 10000 + 500 * (perf.audience - 20);
+          if (aPerformance.audience > 20) {
+            result += 10000 + 500 * (aPerformance.audience - 20);
           }
-          result += 300 * perf.audience;
+          result += 300 * aPerformance.audience;
           break;
         default:
-            throw new Error(`unknown type: ${playFor(perf).type}`);
+            throw new Error(`unknown type: ${playFor(aPerformance).type}`);
     }
 
     return result
@@ -61,10 +61,10 @@ const invoices = [
         }).format(val/100);
   }
 
-  function volumeCreditFor(perf){
-    let result = Math.max(perf.audience - 30, 0);
+  function volumeCreditFor(aPerformance){
+    let result = Math.max(aPerformance.audience - 30, 0);
     // add extra credit for every ten comedy attendees
-    if ("comedy" === playFor(perf).type) result = Math.floor(perf.audience / 5);
+    if ("comedy" === playFor(aPerformance).type) result = Math.floor(aPerformance.audience / 5);
     return result
   }
 
